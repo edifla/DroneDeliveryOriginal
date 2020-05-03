865931a0a9da9e617f6ff0efd3a101394a221f6f
Rafael Albuquerque <54068660+edifla@users.noreply.github.com>
2020-05-03 12:36:46 -0300

    Update 2020_04_25_202340_create_restaurantes_table.php

--- database/migrations/2020_04_25_202340_create_restaurantes_table.php
+++ database/migrations/2020_04_25_202340_create_restaurantes_table.php
@@ -24,6 +24,7 @@ class CreateRestaurantesTable extends Migration
             $table->string('phoneRest');
             $table->longText('descricao');
             $table->string('endereco_restaurante');
+            $table->string('categoria_restaurante');
         });
     }
 
